8c8b3d725f787385a353d03c7dbc3c009b6a8ed1
itapi <itapi@DESKTOP-ABBQ4SF>
2016-09-16 10:52:31 +0300

    Add project files.

---
+++ IshareTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IshareTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
